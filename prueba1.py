89212ff82bbcd6d0352ec86e8f32f80fa03ee69b
unknown <Josephsantacastro@estudiantec.cr>
2021-07-17 01:20:35 -0600

    RANGE

---
+++ prueba1.py
@@ -0,0 +1,27 @@
+m = int ( input("INGRESE UN NUMERO:"))
+
+for n in range(0 , 10):
+
+
+    #repetir 10 veces 
+
+    for i in range(11):
+
+        
+
+        print(n   ,"x" ,i ,"=" , n * i )
+
+
+    print("---------------------------------")
+
+    print("TABLA DE " , n )
+
+
+
+
+
+
+
+
+
+
